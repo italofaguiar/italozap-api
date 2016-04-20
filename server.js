87b3ef33f04151669757cdaaed0f3a54834af3f0
italoaguiar <italofaguiar@gmail.com>
2016-04-19 23:45:08 -0300

    t10

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
         process.env.OPENSHIFT_APP_NAME;
 }
 
-console.log("conectando no mongo: " + mongodbUrl);
+console.log("conectando no mongodb: " + mongodbUrl);
 
 db.connect('mongodb://' + mongodbUrl, function (err) {
     if (err) {
