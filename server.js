9e7f6f7b0add391ddf3dcc2b631c4c4ed8ec3c77
italoaguiar <italofaguiar@gmail.com>
2016-04-19 23:38:06 -0300

    t9

--- server.js
+++ server.js
@@ -26,11 +26,22 @@ app.get('/', function(req, res){
 // var port = Number(process.env.PORT || 8200);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8200;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8300;
 
-var mongodbUrl = 'mongodb://italofaguiar:123456@ds017070.mlab.com:17070/notasitalo';
+//var mongodbUrl = 'mongodb://italofaguiar:123456@ds017070.mlab.com:17070/notasitalo';
 
-db.connect(mongodbUrl, function (err) {
+// default to a 'localhost' configuration:
+var mongodbUrl = '127.0.0.1:27017/notasitalo';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    mongodbUrl = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+db.connect('mongodb://' + mongodbUrl, function (err) {
     if (err) {
         console.log('Unable to connect to Mongo.');
         process.exit(1)
